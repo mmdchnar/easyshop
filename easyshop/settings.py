be39161562c99fa840be4376c7a7cb4e254af261
mmdchnar <mmdchnar@gmail.com>
2022-11-25 14:27:47 +0330

    change static folder

--- easyshop/settings.py
+++ easyshop/settings.py
@@ -121,13 +121,9 @@ USE_TZ = True
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_URL = '/static/'
 
-if DEBUG:
-    STATIC_FILE_ROOT = os.path.join(BASE_DIR, "static")
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 
 # Default primary key field type
