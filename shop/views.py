8564c5606292e4a30cb373b6e7556559edb073ef
mmdchnar <mmdchnar@gmail.com>
2022-11-25 21:53:13 +0330

    clear index view

--- shop/views.py
+++ shop/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
-from django.urls import reverse
-from django.http import HttpResponseRedirect, HttpResponse
-
-from requests import get
-
+# from django.urls import reverse
+# from django.http import HttpResponseRedirect, HttpResponse
 
 
 def index(request):
-    name = request.COOKIES.get('name')
-
-    if not name:
-        # do something
-        pass
-
-    if request.method == 'POST':
-        if 'name' in dict(request.POST.items()):
-            if request.POST['name'].replace(' ', '') != '':
-                response = HttpResponseRedirect(reverse('/'))
-                response.set_cookie('name', request.POST['name'])
-                # response.delete_cookie('name')
-                return response
-
     return render(request, 'index.html')
 
 
