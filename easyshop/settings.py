a71061dd67702d578eca5c9185eb1398b61a4432
mmdchnar <mmdchnar@gmail.com>
2022-11-25 14:28:04 +0330

    remove secret key

--- easyshop/settings.py
+++ easyshop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mpyeewjmmxvn9s(n70t8($y0er%jigy^d1ivfk+4276kd8h$x_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
