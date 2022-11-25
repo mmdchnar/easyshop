71d3a76107db188322af59fff8eecbdccb346144
mmdchnar <mmdchnar@gmail.com>
2022-11-25 16:51:31 +0330

    test

--- easyshop/urls.py
+++ easyshop/urls.py
@@ -29,5 +29,5 @@ handler500 = 'shop.views._404'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
