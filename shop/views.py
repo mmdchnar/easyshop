afd61ed1896021024ac98a2393113c98a5db97f4
mmdchnar <mmdchnar@gmail.com>
2022-11-27 01:31:31 +0330

    Use models in index view

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from .models import Category, Product, Image
+from random import shuffle
 # from django.urls import reverse
 # from django.http import HttpResponseRedirect, HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()
+    products = list(Product.objects.all())
+    images = Image.objects.all()
+    
+    shuffle(products)
+
+    return render(request, 'index.html', {
+        'categories': categories,
+        'products':products,
+        'images': images
+        })
 
 
 def _404(request, ex = None):
