00dbdb25fb9f83040d802417aa550a4efc3bda2e
mmdchnar <mmdchnar@gmail.com>
2022-11-27 01:30:14 +0330

    Make admin view for category, product and image

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Product, Image
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Category', {"fields": (['name', 'link']),}),
+    )
+    list_display = ('name', 'link')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Product', {"fields": (['name', 'link', 'price', 'category', 'image']),}),
+    )
+    list_display = ('name', 'price', 'link', 'category', 'image')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Image', {"fields": (['name', 'link', 'category']),}),
+    )
+    list_display = ('name', 'link', 'category')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Image, ImageAdmin)
