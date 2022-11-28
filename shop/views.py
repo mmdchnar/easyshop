4d9ceb514b90e90f72e76f5640a19fe3e3576139
mmdchnar <mmdchnar@gmail.com>
2022-11-29 00:15:26 +0330

    Export headers, special_off, and off_price to index template

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Mobile, Laptop
+from .models import Mobile, Laptop, Header, Special_Off
 from random import shuffle
 # from django.urls import reverse
 # from django.http import HttpResponseRedirect, HttpResponse
@@ -12,16 +12,29 @@ def _404(request, ex = None):
 
 def index(request):
     mobiles = list(Mobile.objects.all())
-    laptops = list(Laptop.objects.all())
-    products = mobiles + laptops
     shuffle(mobiles)
+
+    laptops = list(Laptop.objects.all())
     shuffle(laptops)
+
+    products = list(Mobile.objects.order_by('-views')[:3])
+    products += list(Laptop.objects.order_by('-views')[:3])
     shuffle(products)
+
+    headers = Header.objects.all()
+
+    special_off = Special_Off.objects.all().first()
+    off_price = int(special_off.price * (100 - special_off.off) / 100)
+    time_left = 1531.30 # now - special_off.time
 
     return render(request, 'index.html',{
         'mobiles': mobiles[:5],
         'laptops': laptops[:5],
         'products': products[:5],
+        'headers': headers,
+        'special_off': special_off,
+        'off_price': off_price,
+        'time_left': time_left,
         })
 
 
@@ -41,6 +54,10 @@ def product(request, url):
     
     else:
         _404(request)
+
+
+    product.views += 1
+    product.save()
 
     shuffle(products)
 
