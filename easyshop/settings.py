e0262af88286ceecf282f98e6af0d59ed256a0ea
mmdchnar <mmdchnar@gmail.com>
2022-11-29 00:09:54 +0330

    Add mathfilters to INSTALLED_APPS

--- easyshop/settings.py
+++ easyshop/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'shop',
     'django_mysql',
+    'mathfilters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
