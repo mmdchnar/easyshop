3544968a6bbde0f0dae91fd4474f68aeb14e3439
mmdchnar <mmdchnar@gmail.com>
2022-11-28 13:22:06 +0330

    Make index and product view

--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,51 @@
-from django.shortcuts import render
-from .models import Category, Product, Image
+from django.shortcuts import render, get_object_or_404
+from .models import Mobile, Laptop
 from random import shuffle
 # from django.urls import reverse
 # from django.http import HttpResponseRedirect, HttpResponse
 
 
-def index(request):
-    categories = Category.objects.all()
-    products = list(Product.objects.all())
-    images = Image.objects.all()
-    
-    shuffle(products)
-
-    return render(request, 'index.html', {
-        'categories': categories,
-        'products':products,
-        'images': images
-        })
-
 
 def _404(request, ex = None):
     return render(request, '404.html')
+
+
+def index(request):
+    mobiles = list(Mobile.objects.all())
+    laptops = list(Laptop.objects.all())
+    products = mobiles + laptops
+    shuffle(mobiles)
+    shuffle(laptops)
+    shuffle(products)
+
+    return render(request, 'index.html',{
+        'mobiles': mobiles[:5],
+        'laptops': laptops[:5],
+        'products': products[:5],
+        })
+
+
+def product(request, url):
+
+    mobile = Mobile.objects.filter(link=url+'/')
+    laptop = Laptop.objects.filter(link=url+'/')
+
+    if len(mobile):
+        product = mobile[0]
+        products = list(Mobile.objects.all())
+
+    elif len(laptop):
+        product = laptop[0]
+        products = list(Laptop.objects.all())
+        
+    
+    else:
+        _404(request)
+
+    shuffle(products)
+
+    return render(request, 'product.html', {
+        'product': product,
+        'products': products[:5],
+        })
+
