3dfb01800a393b0ff26d1e2ca6c2954950221853
mmdchnar <mmdchnar@gmail.com>
2022-11-29 00:12:27 +0330

    Add Header and Special_Off to admin page

--- shop/admin.py
+++ shop/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.forms import ModelForm
 from django.utils.html import format_html
-from .models import Mobile, Laptop, Category, Brand
+from .models import Mobile, Laptop, Category, Brand, Header, Special_Off
 
 
 
 class MobileAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Mobile', {"fields": [
+        ('Mobile:', {"fields": [
             'name',
             'link',
             'category',
@@ -20,17 +20,17 @@ class MobileAdmin(admin.ModelAdmin):
             ],}),
         ('Images', {"fields": ['images']}),
     )
-    list_display = ('name', 'brand', 'price', 'custom_link_field')
+    list_display = ('name', 'brand', 'price', 'Link')
 
-    def custom_link_field(self, obj):
+    def Link(self, obj):
         return format_html('<a href="/product/{0}">{0}</a>', obj.link)
     
-    custom_link_field.allow_tag = True
+    Link.allow_tag = True
 
 
 class LaptopAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Laptop', {"fields": ([
+        ('Laptop:', {"fields": ([
             'name',
             'link',
             'category',
@@ -45,28 +45,81 @@ class LaptopAdmin(admin.ModelAdmin):
             ]),}),
         ('Images', {"fields": ['images']},),
     )
-    list_display = ('name', 'brand', 'price', 'link_field')
+    list_display = ('name', 'brand', 'price', 'Link')
 
-    def link_field(self, obj):
+    def Link(self, obj):
         return format_html('<a href="/product/{0}">{0}</a>', obj.link)
-    link_field.allow_tag = True
+    
+    Link.allow_tag = True
 
 
 class CategoryAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Category', {"fields": (['name', 'link']),}),
+        ('Category:', {"fields": (['name', 'link']),}),
     )
-    list_display = ('name', 'link')
+    list_display = ('name', 'Link')
+
+    def Link(self, obj):
+        return format_html('<a href="/product/{0}">{0}</a>', obj.link)
+    
+    Link.allow_tag = True
 
 
 class BrandAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Laptop', {"fields": (['name', 'link']),}),
+        ('Brand:', {"fields": (['name', 'link']),}),
     )
-    list_display = ('name', 'link')
+    list_display = ('name', 'Link')
+
+    def Link(self, obj):
+        return format_html('<a href="/product/{0}">{0}</a>', obj.link)
+    
+    Link.allow_tag = True
+
+
+class HeaderAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Header:', {"fields": (['title', 'link', 'sub_title', 'description', 'image']),}),
+    )
+    list_display = ('title', 'image', 'Link')
+
+    def Link(self, obj):
+        return format_html('<a href="/product/{0}">{0}</a>', obj.link)
+    
+    Link.allow_tag = True
+
+
+class Special_OffAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Special OFF:', {"fields": ([
+            'title',
+            'link',
+            'image',
+            'time',
+            'price',
+            'off',
+            'detail_1',
+            'detail_2',
+            'detail_3',
+            ]),}),
+    )
+    list_display = ('title', 'off', 'time', 'Link')
+
+    def Link(self, obj):
+        return format_html('<a href="/product/{0}">{0}</a>', obj.link)
+    
+    Link.allow_tag = True
+
+    def has_add_permission(self, request):
+        retVal = super().has_add_permission(request)
+        if retVal and Special_Off.objects.exists():
+            retVal = False
+        return retVal
 
 
 
+admin.site.register(Special_Off, Special_OffAdmin)
+admin.site.register(Header, HeaderAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Mobile, MobileAdmin)
