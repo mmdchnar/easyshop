f2ccda1d8d6e846586b142b66471c77f753f5fd3
mmdchnar <mmdchnar@gmail.com>
2022-11-28 13:21:03 +0330

    Change models method

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,55 @@
 from django.contrib import admin
-from .models import Category, Product, Image
+from django.forms import ModelForm
+from django.utils.html import format_html
+from .models import Mobile, Laptop, Category, Brand
+
+
+
+class MobileAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Mobile', {"fields": [
+            'name',
+            'link',
+            'category',
+            'brand',
+            'price',
+            'size',
+            'display_type',
+            'camera_resolution',
+            'os',
+            ],}),
+        ('Images', {"fields": ['images']}),
+    )
+    list_display = ('name', 'brand', 'price', 'custom_link_field')
+
+    def custom_link_field(self, obj):
+        return format_html('<a href="/product/{0}">{0}</a>', obj.link)
+    
+    custom_link_field.allow_tag = True
+
+
+class LaptopAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Laptop', {"fields": ([
+            'name',
+            'link',
+            'category',
+            'brand',
+            'price',
+            'size',
+            'cpu_serie',
+            'ram',
+            'ram_type',
+            'graphic_brand',
+            'resolution',
+            ]),}),
+        ('Images', {"fields": ['images']},),
+    )
+    list_display = ('name', 'brand', 'price', 'link_field')
+
+    def link_field(self, obj):
+        return format_html('<a href="/product/{0}">{0}</a>', obj.link)
+    link_field.allow_tag = True
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -9,19 +59,15 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'link')
 
 
-class ProductAdmin(admin.ModelAdmin):
+class BrandAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Product', {"fields": (['name', 'link', 'price', 'category', 'image']),}),
+        ('Laptop', {"fields": (['name', 'link']),}),
     )
-    list_display = ('name', 'price', 'link', 'category', 'image')
+    list_display = ('name', 'link')
 
 
-class ImageAdmin(admin.ModelAdmin):
-    fieldsets = (
-        ('Image', {"fields": (['name', 'link', 'category']),}),
-    )
-    list_display = ('name', 'link', 'category')
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Image, ImageAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Mobile, MobileAdmin)
+admin.site.register(Laptop, LaptopAdmin)
