24893a9f4fd016d074e0763ac54bde894bf50a1c
mmdchnar <mmdchnar@gmail.com>
2022-11-28 13:21:19 +0330

    Change models method

--- shop/models.py
+++ shop/models.py
@@ -1,33 +1,58 @@
-from django.db import models
+from django.db.models import Model, CharField, SmallIntegerField,\
+    BigIntegerField, ForeignKey, FloatField, IntegerField, CASCADE
+
+from django_mysql.models import ListCharField
 
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=50) # name = 'Mobile'
-    link = models.CharField(max_length=50) # link = 'mobile/'
-
-    def __str__(self):
-        return self.name
-
-
-class Image(models.Model):
-    name = models.CharField(max_length=50) # name = 'IPhone 14 Pro Max'
-    link = models.CharField(max_length=50) # link = 'iphone-14-pro-max.png'
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # category.link = 'mobile/'
+class Category(Model):
+    name = CharField(max_length=50, unique=True)
+    link = CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=50) # name = 'IPhone 14 Pro Max 256GB/6GB'
-    link = models.CharField(max_length=50) # link = 'iphone-14-pro-max-256-6/'
-    price = models.IntegerField() # price = 52600000
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    # category.link = 'mobile/'
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    # image.link = 'iphone-14-pro-max.png'
+class Brand(Model):
+    name = CharField(max_length=50, unique=True)
+    link = CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Mobile(Model):
+    name = CharField(max_length=50, unique=True)
+    link = CharField(max_length=50, unique=True)
+    images = ListCharField(CharField(max_length=255), max_length=255)
+    price = BigIntegerField()
+    count = IntegerField(default=1)
+    brand = ForeignKey(Brand, CASCADE)
+    category = ForeignKey(Category, CASCADE, default=1)
+    size = FloatField()
+    display_type = CharField(max_length=50)
+    camera_resolution = SmallIntegerField()
+    os = CharField(max_length=50)
     
     def __str__(self):
         return self.name
+
+
+class Laptop(Model):
+    name = CharField(max_length=50, unique=True)
+    link = CharField(max_length=50, unique=True)
+    images = ListCharField(CharField(max_length=255), max_length=255)
+    price = BigIntegerField()
+    count = IntegerField(default=1)
+    size = FloatField()
+    brand = ForeignKey(Brand, CASCADE)
+    category = ForeignKey(Category, CASCADE, default=2)
+    cpu_serie = CharField(max_length=50)
+    ram = SmallIntegerField()
+    ram_type = CharField(max_length=50)
+    graphic_brand = CharField(max_length=50)
+    resolution = CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
