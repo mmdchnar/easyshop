6061bd36aca79f653a1919565f21b72e390064a6
mmdchnar <mmdchnar@gmail.com>
2022-12-04 02:17:10 +0330

    Add category view and some clean code

--- shop/views.py
+++ shop/views.py
@@ -12,13 +12,11 @@ def _404(request, ex = None):
 
 
 def index(request):
-    mobiles = list(Mobile.objects.all())
-    shuffle(mobiles)
+    mobiles = Mobile.objects.order_by('?')[:10]
 
-    laptops = list(Laptop.objects.all())
-    shuffle(laptops)
+    laptops = Laptop.objects.order_by('?')[:10]
 
-    products = list(Product.objects.order_by('-views')[:6])
+    products = list(Product.objects.order_by('-views')[:10])
     most_view = []
 
     for product in products:
@@ -41,42 +39,78 @@ def index(request):
     time_left = special_off.time - now()
     time_left = time_left.days * 24*60 + time_left.seconds / 60
 
-    return render(request, 'index.html',{
-        'mobiles': mobiles[:5],
-        'laptops': laptops[:5],
+
+    context = {
+        'mobiles': mobiles,
+        'laptops': laptops,
         'most_view': most_view[:5],
         'images': images,
         'headers': headers,
         'special_off': special_off,
         'off_price': off_price,
         'time_left': time_left,
-        })
+        }
+
+    return render(request, 'index.html', context)
 
 
 def product(request, url):
+    url = url.lower() + '/'
 
-    link = Product.objects.filter(link=url+'/')
+    link = Product.objects.filter(link=url)
 
     if len(link):
-        product = eval(f'{link[0].category.name}.objects.get(product__name=link[0].name)')
-        products = eval(f'list({link[0].category.name}.objects.all())')
+        product = eval(f'{link.first().category.name}.objects.get(product__name=link.first().name)')
+        products = eval(f'{link.first().category.name}.objects.order_by("?")[:10]')
+
+        link.first().views += 1
+        link.first().save()
 
     else:
-        _404(request)
-
-
-    link[0].views += 1
-    link[0].save()
-
-    shuffle(products)
+        return _404(request)
 
     images = Image.objects.all()
 
-    return render(request, 'product.html', {
+
+    context = {
         'product': product,
-        'products': products[:5],
+        'products': products,
         'images': images,
-        })
+        }
+
+    return render(request, 'product.html', context)
 
 
+def category(request, url):
+    url = url.lower() + '/'
 
+    category = Category.objects.filter(link=url)
+    brand = Brand.objects.filter(link=url)
+
+    if len(category):
+        products = eval(f'{category.first().name}.objects.order_by("?")[:21]')
+        group = category.first()
+
+    elif len(brand):
+        products = []
+
+        for cat in Category.objects.all():
+            products += list(eval(f'{cat.name}.objects.filter(product__brand__link=url)[:21]'))
+
+        shuffle(products)
+
+        group = brand.first()
+    
+    else:
+        return _404(request)
+
+    images = Image.objects.all()
+
+
+    context = {
+        'products': products[:21],
+        'images': images,
+        'group': group,
+        }
+
+    return render(request, 'category.html', context)
