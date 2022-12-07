818a5c6ef0a735350ad18c4c719ad0d68f30c79e
mmdchnar <mmdchnar@gmail.com>
2022-12-07 11:55:38 +0330

    Add /search url

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<slug:url>/', views.product, name='product'),
+    path('search/', views.search, name='search'),
     path('<slug:url>/', views.category, name='category'),
 ]
