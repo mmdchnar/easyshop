b1aa28de8aca54e34f17ac72bfbe9c48c521e64b
mmdchnar <mmdchnar@gmail.com>
2022-12-07 11:54:51 +0330

    Add postgres to INSTALLED_APPS

--- easyshop/settings.py
+++ easyshop/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'shop',
     'mathfilters',
+    'django.contrib.postgres',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
