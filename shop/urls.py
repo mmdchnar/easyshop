fe18324cef8bfb354ae722cb6beb60abbcfe3183
mmdchnar <mmdchenar@gmail.com>
2022-12-10 00:16:15 +0330

    Change view name

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<slug:url>/', views.product, name='product'),
     path('search/', views.search, name='search'),
-    path('<slug:url>/', views.category, name='category'),
+    path('<slug:url>/', views.categories_and_brands, name='category'),
 ]
