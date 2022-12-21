e2c342b097c28a04746d9238c2257d41502a90e2
mmdchnar <mmdchenar@gmail.com>
2022-12-21 13:06:56 +0330

    Define login view

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,12 @@
-from .models import Mobile, Laptop, Header, SpecialOff, Product, Image, Category, Brand, Detail
+from .models import Mobile, Laptop, Header, SpecialOff,\
+     Product, Image, Category, Brand, Detail, Account, Session
 from django.contrib.postgres.search import SearchVector
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.timezone import now
+from django.db.utils import IntegrityError
 from random import shuffle
+from secrets import token_urlsafe
 
 # from django.urls import reverse
 # from django.http import HttpResponseRedirect, HttpResponse
@@ -14,6 +17,11 @@ def _404(request, ex=None):
 
 
 def index(request):
+    try:
+        account = Session.objects.get(session=request.session['user-session']).user
+    except (ObjectDoesNotExist, KeyError):
+        account = None
+
     mobiles = Mobile.objects.order_by('?')[:10]
 
     laptops = Laptop.objects.order_by('?')[:10]
@@ -44,6 +52,7 @@ def index(request):
     time_left = time_left.days * 24 * 60 + time_left.seconds / 60
 
     context = {
+        'account': account,
         'mobiles': mobiles,
         'laptops': laptops,
         'most_view': most_view[:5],
@@ -73,7 +82,13 @@ def product(request, url):
     brands = Brand.objects.all()
     categories = Category.objects.all()
 
+    try:
+        account = Session.objects.get(session=request.session['user-session']).user
+    except (ObjectDoesNotExist, KeyError):
+        account = None
+
     context = {
+        'account': account,
         'title': __product.product,
         'product': __product,
         'products': products,
@@ -87,8 +102,6 @@ def product(request, url):
 
 
 def categories_and_brands(request, url):
-    # url = url.lower() + '/'
-
     try:
         group = Category.objects.get(name__iexact=url)
         products = eval(f'{group.name}.objects.order_by("?")[:21]')
@@ -104,7 +117,13 @@ def categories_and_brands(request, url):
     brands = Brand.objects.all()
     categories = Category.objects.all()
 
+    try:
+        account = Session.objects.get(session=request.session['user-session']).user
+    except (ObjectDoesNotExist, KeyError):
+        account = None
+
     context = {
+        'account': account,
         'title': 'محصولات ' + group.persian_name,
         'products': products[:21],
         'images': images,
@@ -117,7 +136,7 @@ def categories_and_brands(request, url):
 
 
 def search(request):
-    query = request.GET.get("q")
+    query = request.GET.get('q')
 
     if not query:
         return _404(request)
@@ -141,8 +160,14 @@ def search(request):
     brands = Brand.objects.all()
     categories = Category.objects.all()
 
+    try:
+        account = Session.objects.get(session=request.session['user-session']).user
+    except (ObjectDoesNotExist, KeyError):
+        account = None
+
     context = {
-        'title': 'نتیجه جستجو',
+        'account': account,
+        'title': 'نتایج جستجو',
         'products': products[:21],
         'images': images,
         'brands': brands,
@@ -151,3 +176,75 @@ def search(request):
     }
 
     return render(request, 'custom.html', context)
+
+
+def login(request):
+    redir = request.GET.get("redirect") if request.GET.get("redirect") else '/'
+
+    try:
+        Session.objects.get(session=request.session['user-session'])
+        return redirect(redir)
+    except (ObjectDoesNotExist, KeyError):
+        pass
+    
+    brands = Brand.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        'error': '',
+        'brands': brands,
+        'categories': categories,
+        }
+    
+    if request.method == 'POST':
+        _name = request.POST.get('name')
+        _pass = request.POST.get('password')
+        _email = request.POST.get('email')
+
+        token = token_urlsafe()
+
+        if _name:
+            if _name.replace(' ','') == '':
+                context['error'] = 'نام و نام خوانوادگی خالی است!'
+                return render(request, 'login.html', context)
+
+            elif len(_pass) < 8 or ' ' in _pass or \
+                (_pass.lower() != _pass != _pass.upper() and not any([c.isdigit() for c in _pass])):
+                context['error'] = 'کلمه عبور باید بدون فاصله و ترکیبی از اعداد و حروف کوچک و بزرگ و حداقل ۸ کاراکتر باشد!'
+                return render(request, 'login.html', context)
+
+            try:
+                account = Account(
+                    name=_name,
+                    email=_email,
+                    password=_pass,
+                    )
+                account.save()
+
+            except IntegrityError:
+                context['error'] = 'اکانت با این ایمیل موجود است لطفا وارد شوید!'
+                return render(request, 'login.html', context)
+
+        else:
+            try:
+                account = Account.objects.get(email=_email)
+                if account.password != _pass:
+                    context['error'] = 'کلمه عبور اشتباه است!'
+                    return render(request, 'login.html', context)
+
+            except ObjectDoesNotExist:
+                context['error'] = 'ایمیل موجود نیست! لطفا اکانت بسازید.'
+                return render(request, 'login.html', context)
+
+        session = Session(
+                user=account,
+                session=token,
+                description=request.META['HTTP_USER_AGENT'],
+                )
+        session.save()
+
+        request.session['user-session'] = token
+
+        return redirect(redir)
+
+    return render(request, 'login.html', context)
