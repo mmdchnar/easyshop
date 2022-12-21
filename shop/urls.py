5155d031b1c8d68a8de675f3b5473a4d7dffb927
mmdchnar <mmdchenar@gmail.com>
2022-12-21 11:54:36 +0330

    Add login url to urls

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('product/<str:url>/', views.product, name='product'),
     path('search/', views.search, name='search'),
+    path('login/', views.login, name='login'),
+    path('product/<str:url>/', views.product, name='product'),
     path('<slug:url>/', views.categories_and_brands, name='category'),
 ]
