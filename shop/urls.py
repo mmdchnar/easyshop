455734a719f794429b8c43fda4e7897bbc5f4bb0
mmdchnar <mmdchenar@gmail.com>
2022-12-24 03:27:42 +0330

    Add /cart to urls

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     path('login/', views.login, name='login'),
+    path('cart/', views.cart, name='cart'),
     path('product/<str:url>/', views.product, name='product'),
     path('<slug:url>/', views.categories_and_brands, name='category'),
 ]
